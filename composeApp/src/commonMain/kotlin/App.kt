2c4676e93305523a2499f899a8dc385e11a8e079
Santiago Mattiauda <santimattius@gmail.com>
2024-11-17 14:03:48 -0300

    [update] update dependencies

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,12 +1,10 @@
 import androidx.compose.runtime.Composable
 import coil3.ImageLoader
-import coil3.annotation.ExperimentalCoilApi
 import coil3.compose.setSingletonImageLoaderFactory
-import coil3.network.ktor.KtorNetworkFetcherFactory
+import coil3.network.ktor3.KtorNetworkFetcherFactory
 import com.santimattius.kmp.delivery.MainApplication
 import com.santimattius.kmp.delivery.core.ui.themes.AppTheme
 
-@OptIn(ExperimentalCoilApi::class)
 @Composable
 fun App() {
     setSingletonImageLoaderFactory { context ->
